3beb48194d9553d8e2d57b0be6499155fac3a708
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-09 00:17:50 +0800

    link data to buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -12,20 +13,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          'The question...',
-          style: TextStyle(
+        Text(
+          currentQuestion.text,
+          style: const TextStyle(
             color: Colors.white,
           ),
         ),
         const SizedBox(height: 30),
-        AnswerButton(answerText: 'Answer 1', onTap: () {}),
-        AnswerButton(answerText: 'Answer 2', onTap: () {}),
-        AnswerButton(answerText: 'Answer 3', onTap: () {}),
-        AnswerButton(answerText: 'Answer 4', onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
       ]),
     );
   }
