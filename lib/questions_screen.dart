86ffd49c1f3075be148e546f3f26bd1c66e513b9
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-08 23:09:25 +0800

    center questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,13 +12,16 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Column(children: [
-      const Text('The question...'),
-      const SizedBox(height: 30),
-      ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
-      ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
-      ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
-      ElevatedButton(onPressed: () {}, child: const Text('Answer 4')),
-    ]);
+    return SizedBox(
+      width: double.infinity,
+      child: Column(children: [
+        const Text('The question...'),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 4')),
+      ]),
+    );
   }
 }
