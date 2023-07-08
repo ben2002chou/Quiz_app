b488d5d14ec7704d702680c4954e542ee542c4f9
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-08 23:39:11 +0800

    Answer button functionality

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  AnswerButton(this.answerText, {super.key});
+  const AnswerButton(this.answerText, this.onTap, {super.key});
 
   final String answerText;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {},
-      child: const Text('Answer 1'),
+      child: Text(answerText),
     );
   }
 }
