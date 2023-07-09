abb68601eb35e3efa0f52707ae86c55e6a86aef4
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-09 17:15:51 +0800

    link results screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -37,7 +38,7 @@ class _QuizState extends State<Quiz> {
       
       if(selectedAnswers.length == questions.length) {
         selectedAnswers.clear();
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       }
     });
   }
@@ -52,6 +53,11 @@ class _QuizState extends State<Quiz> {
     if (activeScreen == 'questions-screen') {
       screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
     }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = const ResultsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
