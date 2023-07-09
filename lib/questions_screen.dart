27908b5fd91302657ebae666b5de5f2a653c263e
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-09 10:38:32 +0800

    stretch out buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -17,22 +17,26 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-          currentQuestion.text,
-          style: const TextStyle(
-            color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
           ),
-        ),
-        const SizedBox(height: 30),
-        //...spreads out list items
-        ...currentQuestion.answers.map((answer) {
-          return AnswerButton(
-            answerText: answer,
-            onTap: () {},
-          );
-        }),
-      ]),
+          const SizedBox(height: 30),
+          //...spreads out list items
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(
+              answerText: answer,
+              onTap: () {},
+            );
+          }),
+        ],
+      ),
     );
   }
 }
