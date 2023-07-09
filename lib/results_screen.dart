c6b886357fd948264a9acfffcec986249e4f9c1e
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-09 17:13:25 +0800

    fix bugs

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
+
+  const ResultsScreen({super.key});  
   @override
   Widget build(BuildContext context) {
     return SizedBox(
