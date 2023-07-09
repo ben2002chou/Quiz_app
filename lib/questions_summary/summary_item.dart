a2c92895cd068a0b741203c014926733d55bc115
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-10 01:44:40 +0800

    add padding

--- lib/questions_summary/summary_item.dart
+++ lib/questions_summary/summary_item.dart
@@ -14,36 +14,39 @@ class SummaryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isCorrectAnswer = itemData['user_answer'] == itemData['correct_answer'];
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        QuestionIdentifier(isCorrectAnswer: isCorrectAnswer, questionIndex: itemData['question_index'] as int,),
-        const SizedBox(
-          width: 20,
-        ),
-        Expanded(child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // align to top of the row
-          children: [
-            Text(itemData['question'] as String, style: GoogleFonts.lato(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuestionIdentifier(isCorrectAnswer: isCorrectAnswer, questionIndex: itemData['question_index'] as int,),
+          const SizedBox(
+            width: 20,
+          ),
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // align to top of the row
+            children: [
+              Text(itemData['question'] as String, style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(itemData['user_answer'] as String, style: const TextStyle(
-              color: Color.fromARGB(255, 246, 222, 249),
-            ),),
-            Text(itemData['correct_answer'] as String, style: const TextStyle(
-              color: Color.fromARGB(255, 216, 246, 248),
-            ),),
-            
-          ],
-        ),)
-
-      ],
+              const SizedBox(
+                height: 5,
+              ),
+              Text(itemData['user_answer'] as String, style: const TextStyle(
+                color: Color.fromARGB(255, 246, 222, 249),
+              ),),
+              Text(itemData['correct_answer'] as String, style: const TextStyle(
+                color: Color.fromARGB(255, 216, 246, 248),
+              ),),
+              
+            ],
+          ),)
+    
+        ],
+      ),
     );
   }
 }
