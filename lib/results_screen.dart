d1f5d60ef6e54b78a77c31a20897e9cbcdda1271
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-09 20:42:13 +0800

    add quiz summary mapping function

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
@@ -7,6 +8,22 @@ class ResultsScreen extends StatelessWidget {
   });
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'corrrct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
