be7979d1dcfae5d8e0852891478e127694a1013c
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-09 12:03:50 +0800

    add fonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 
@@ -35,8 +37,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(
-                color: Colors.white,              
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 243, 231, 246),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
